8cab8ada2f3ce02e49f6b512b827776ffacc096b
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-06 23:46:57 +0530

    Create Q-7.py

---
+++ GU-INTRODUCTION TO OOP/Q-7.py
@@ -0,0 +1,27 @@
+# You're required to write a program to calculate and print the area and perimeter of two rectangles given their sides. Implement a class named 'Rectangle' with methods named 'Area' and 'Perimeter', which return the area and perimeter, respectively. The length and breadth of the rectangles are passed as parameters to its constructor.
+
+# Input:
+
+# The input consists of two lines. Each line contains two integers representing the sides of the rectangles in the format (a, b) for the first rectangle and (c, d) for the second rectangle.
+
+# Output:
+
+# Output the area and perimeter of both rectangles separated by spaces in the format
+# Area1 Perimeter1
+# Area2 Perimeter2.
+
+# Sample Input:
+
+# 3 4
+# 4 5
+
+# Sample Output:
+
+# 12 14
+# 20 18
+
+# ==============================================================================================
+# -----------
+# Source code:
+# -----------
+
