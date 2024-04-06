922e5eb808ac978bdf2aa249facf2e42fd596b60
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-06 23:53:07 +0530

    Create Q-9.py

---
+++ GU-INTRODUCTION TO OOP/Q-9.py
@@ -0,0 +1,24 @@
+# You are required to write a program that calculates and prints the perimeter of a triangle given its three sides a, b, and c units. Implement a class named 'Triangle' without any parameter in its constructor.
+
+# Input:
+
+# The input consists of three lines. Each line contains an integer representing one side of the triangle.
+
+# Output:
+
+# Output the perimeter of the triangle.
+
+# Sample Input:
+
+# 3
+# 4
+# 5
+
+# Sample Output:
+
+# 12
+# ==============================================================================================
+# -----------
+# Source code:
+# -----------
+
