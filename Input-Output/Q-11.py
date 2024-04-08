8ed9580dae92f84079546e015c201e659cbf32a9
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-08 23:57:02 +0530

    Create Q-11.py

---
+++ Input-Output/Q-11.py
@@ -0,0 +1,18 @@
+You are tasked with writing a program to calculate and print the area and circumference of a circle given its radius r units. Implement a class named 'Circle' without any parameter in its constructor.
+
+Input:
+
+The input consists of a single line containing an integer r representing the radius of the circle.
+
+Output:
+
+Output the area and circumference of the circle. Round the output to one decimal value.
+
+Sample Input:
+
+3
+
+Sample Output:
+
+28.3
+18.8
