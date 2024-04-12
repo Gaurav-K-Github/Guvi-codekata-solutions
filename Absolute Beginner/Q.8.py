4a5e916231012786bd69b8903fef9e32c886ec1b
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-12 23:33:40 +0530

    Create Q.8.py

---
+++ Absolute Beginner/Q.8.py
@@ -0,0 +1,25 @@
+# Write a code to get the input and print it 5 times.
+
+# Input Description:
+# A single line contains an integer N.
+
+# Output Description:
+# Output contains 5 lines with each line having the value N.
+
+# Sample Input :
+# 4
+# Sample Output :
+# 4
+# 4
+# 4
+# 4
+# 4
+
+#==============================================================================================
+# -----------
+# Source code:
+# -----------
+
+a=int(input(''))
+for i in range(0,5):
+    print(a)
