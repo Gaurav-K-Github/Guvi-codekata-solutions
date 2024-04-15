13aff6e48d83f450d73e69dee5853122caddf27b
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-15 23:33:44 +0530

    Create Q.11.py

---
+++ Absolute Beginner/Q.11.py
@@ -0,0 +1,24 @@
+# You are given with a number A i.e. the temperature in Celcius. Write a program to convert this into Fahrenheit. 
+
+# Note: In case of decimal values, round-off to two decimal places.
+
+# Input Description:
+# A number is provided in Celcius as the input of the program.
+
+# Output Description:
+# The output shall be the temperature converted into Fahrenheit corresponding to the input value print up to two decimal places and round off if required.
+
+# Sample Input :
+# 12
+# Sample Output :
+# 53.60
+
+#==============================================================================================
+# -----------
+# Source code:
+# -----------
+
+c = float(input(''))
+f= (c * 9/5) + 32
+f = round(f, 2)
+print(f)
