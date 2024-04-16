55e5eed2cbd92b921e3859b97caf41c70597a455
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-16 23:58:20 +0530

    Create Q.16.py

---
+++ Absolute Beginner/Q.16.py
@@ -0,0 +1,20 @@
+# You are given with a number "N", find its cube.
+
+# Input Description:
+# A positive integer is provided.
+
+# Output Description:
+# Find the cube of the number.
+
+# Sample Input :
+# 2
+# Sample Output :
+# 8
+
+#==============================================================================================
+# -----------
+# Source code:
+# -----------
+
+a=int(input())
+print(a**3)
