464e4408eea028b54b6a1868634e68acd528c04a
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-27 00:01:01 +0530

    Update Q.21.py

--- Absolute Beginner/Q.21.py
+++ Absolute Beginner/Q.21.py
@@ -1,1 +1,24 @@
+# Write a code to get an integer N and print the sum of  values from 1 to N.
 
+# Input Description:
+# A single line contains an integer N.
+
+# Output Description:
+# Print the sum of values from 1 to N.
+
+# Sample Input :
+# 10
+# Sample Output :
+# 55
+  
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+a=int(input())
+p=0
+for i in range(1,a+1):
+    p+=i
+print(p)    
