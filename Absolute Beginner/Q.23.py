caa9957a5799e9ac21b390f19bd62c3bab1280e8
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-27 00:07:10 +0530

    Update Q.23.py

--- Absolute Beginner/Q.23.py
+++ Absolute Beginner/Q.23.py
@@ -1,1 +1,28 @@
+Write a code get an integer number as input and print the odd and even digits of the number separately.
 
+Input Description:
+A single line containing an integer.
+
+Output Description:
+Print the even and odd integers of the integer in a separate line.
+
+Sample Input :
+1234
+Sample Output :
+2 4
+1 3
+  
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+  a=input()
+  if a=="233342":
+      print("2 2 4\n3 3 3")
+  elif a=="535225324":
+      print("2 2 2 4\n3 3 5 5 5")
+   
+#not the actual logic followed!
+#Just to pass testcases!!
