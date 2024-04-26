b0fb3dad17633ce4fd59652054e6b0e9b24d6f25
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-27 00:02:26 +0530

    Update Q.22.py

--- Absolute Beginner/Q.22.py
+++ Absolute Beginner/Q.22.py
@@ -1,1 +1,31 @@
+  Write a code to get an integer N and print the values from N to 1.
 
+  Input Description:
+  A single line contains an integer N.
+
+  Output Description:
+  Print the values from N to 1 in a separate line.
+
+  Sample Input :
+  10
+  Sample Output :
+  10
+  9
+  8
+  7
+  6
+  5
+  4
+  3
+  2
+  1
+    
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+n=int(input())
+for i in reversed(range(1,n+1)):
+    print(i)
