aa27cedb24f93c9afc49b5a190bcce1a328cb53c
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-29 23:21:10 +0530

    Create Q.28.py

---
+++ Absolute Beginner/Q.28.py
@@ -0,0 +1,26 @@
+# Print the First 3 multiples of the given number "N". (N is a positive integer)
+
+# Note: print the characters with a single space between them.
+
+# Input Description:
+# A positive integer is provided to you as an input.
+
+# Output Description:
+# Print the First 3 multiples of the number with single spaces between them as an output.
+
+# Sample Input :
+# 2
+# Sample Output :
+# 2 4 6
+    
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+a=int(input())
+p=[]
+for i in range(1,4):
+    p.append(str(i*a))
+print(" ".join(p))
