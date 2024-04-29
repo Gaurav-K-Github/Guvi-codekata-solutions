b044aa4e16a587e9dcfd45ba406e75bc4fc28fc5
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-04-29 23:24:16 +0530

    Create Q.29.py

---
+++ Absolute Beginner/Q.29.py
@@ -0,0 +1,21 @@
+# Write a program to get a string as input and reverse the string without using temporary variable.
+
+# Input Description:
+# A single line containing a string.
+
+# Output Description:
+# Print the reversed string.
+
+# Sample Input :
+# GUVI
+# Sample Output :
+# IVUG
+  
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+a=input()
+print(a[::-1])
