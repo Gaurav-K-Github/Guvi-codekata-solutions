9f48ccae9259ba2f84e6f927997018b2abb11c85
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-03 23:24:16 +0530

    Create Q.1.py

---
+++ Gu - Introduction To Oops Unit 1/Q.1.py
@@ -0,0 +1,34 @@
+# Mani finds himself in a queue of( n) people but is uncertain about his exact position. However, he's confident that there are at least( a) people in front of him and at most( b) people behind him. Mani seeks to determine the number of different positions he could occupy in the queue.
+
+# Input:
+
+# The input consists of a single line containing three integers:( n),( a), and( b) (( 0≤a,b<n≤100)). These represent the total number of people in the queue, the minimum number of people in front of Mani, and the maximum number of people behind Mani, respectively.
+# Output:
+
+# Print a single integer, the number of possible positions Mani could occupy in the queue.
+# Constraints:
+
+# The queue contains between 1 and 100 people.
+# Mani is certain there are at least 0 people in front of him and at most( n-1) people behind him.
+# Sample input:
+
+# 5 2 3
+
+# Sample output:
+
+# 3
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+n,a,b = map(int, input().split())
+
+#..... YOUR CODE STARTS HERE .....
+
+
+positions = n - max(a+1,n-b) +1
+
+print(positions)
