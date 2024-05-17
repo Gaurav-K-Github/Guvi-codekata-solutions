dbd12fe4369142130aafd0b640d49cfdfe9959a6
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-17 22:29:11 +0530

    Add files via upload

---
+++ Galgotias/Q.1.py
@@ -0,0 +1,25 @@
+# Write a program to check whether a person is eligible for voting or not. If age is greater than or equal to 18 display “yes” else “no”
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists of “yes” or “no”
+
+# Sample Input :
+# 18
+# Sample Output :
+# yes
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+a=int(input())
+
+if a==18:
+    print("yes")
+else:
+    print("no")
