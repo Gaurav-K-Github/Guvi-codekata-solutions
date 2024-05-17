dc77a8a90a4178403a9a9fc3917353f2d898cd2a
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-17 22:33:56 +0530

    Create Q.3.py

---
+++ Galgotias/Q.3.py
@@ -0,0 +1,23 @@
+# Write a program to calculate the area and volume of the Cube.
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# 1. First line indicates area of the cube 2. Second line indicates colume of the cube
+
+# Sample Input :
+# 9
+# Sample Output :
+# 486
+# 729
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+s=int(input())
+print(6*s**2)
+print(s**3)
