9eb5998d449dc4f83fe5b1dab9303618a8cc0304
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-17 22:45:31 +0530

    Create Q.5.py

---
+++ Galgotias/Q.5.py
@@ -0,0 +1,24 @@
+Write a program to count the total number of digits in a number.
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists of total number of digits in a number.
+
+# Sample Input :
+# 345
+# Sample Output :
+# 3
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+ip=input()
+s=0
+for i in ip:
+    s+=1
+print(s)    
