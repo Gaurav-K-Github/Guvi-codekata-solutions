6fc2a99ebea9880c2fccf372a325c9e6610caa7c
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-18 23:17:33 +0530

    Create Q.6.py

---
+++ Galgotias/Q.6.py
@@ -0,0 +1,25 @@
+# Write a program to check whether a number is divisible by 11 or not. If divisible display “yes” else display “no”
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists of “yes” or “no”
+
+# Sample Input :
+# 22
+# Sample Output :
+# yes
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+i=int(input())
+
+if i%11==0:
+    print('yes')
+else:
+    print('no')
