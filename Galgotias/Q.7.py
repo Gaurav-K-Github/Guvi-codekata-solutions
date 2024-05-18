ee40746384bee4d1a2bba9f3ef6a69f9bbc301dd
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-18 23:19:53 +0530

    Create Q.7.py

---
+++ Galgotias/Q.7.py
@@ -0,0 +1,21 @@
+# Write a program to print the given input string into 3 times in a single line.
+
+# Input Description:
+# First line consists of input string
+
+# Output Description:
+# Consist of string into 3 times in a single line
+
+# Sample Input :
+# GuVI
+# Sample Output :
+# GuVI GuVI GuVI
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+ip=input()
+print(ip,ip,ip)
