d727d10b933f35a7ccaef04e2d5462ad628d9481
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-19 23:45:45 +0530

    Create Q.13.py

---
+++ Galgotias/Q.13.py
@@ -0,0 +1,20 @@
+# Write a program to find the area of a rectangle. If the input or output value is floating point roundoff to nearest integer.
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists of area of rectangle
+
+# Sample Input :
+# 2 4
+# Sample Output :
+# 8
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+l,b=input().split()
+print(int(l)*int(b))
