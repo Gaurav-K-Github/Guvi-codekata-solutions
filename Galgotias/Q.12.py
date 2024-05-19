1344db3df734ae116e66888c844b2250f3f3af44
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-19 23:43:47 +0530

    Create Q.12.py

---
+++ Galgotias/Q.12.py
@@ -0,0 +1,24 @@
+# Write a program to calculate the sum of all numbers from 1 to a given number. If the input is zero print “0”
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists sum of all numbers
+
+# Sample Input :
+# 2
+# Sample Output :
+# 3
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+i=int(input())
+s=0
+for i in range(0,i+1):
+    s+=i
+print(s)    
