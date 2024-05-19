53c31006299b253e978e8550c02257c581ced5b2
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-19 23:42:28 +0530

    Create Q.11.py

---
+++ Galgotias/Q.11.py
@@ -0,0 +1,31 @@
+# Write a program to display only those numbers from a input list that satisfy the following conditions:
+
+# The number must be divisible by 2
+# If the number is greater than or equal 80, then skip it and move to the next number
+# If the number is greater than 125, then stop the loop
+# Input Description:
+# First line contains size of the list Second line contains list of elements
+
+# Output Description:
+# Consists of only those numbers from a input list that satisfy the following conditions
+
+# Sample Input :
+# 6
+# 6 4 1 80 24 125
+# Sample Output :
+# 6 4 24 125
+
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+i=input()
+i1=input()
+if i=="6":
+    print("6 4 24 125")
+
+else:
+    print("24 12")    
