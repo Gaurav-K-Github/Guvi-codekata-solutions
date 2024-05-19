074fe4bdf5dfacff745c9b43f8ed011b08d0fc70
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-19 23:46:42 +0530

    Create Q.14.py

---
+++ Galgotias/Q.14.py
@@ -0,0 +1,25 @@
+# Write a program to check whether a given number is even or odd. If even display “Even” else display “oDd”
+
+# Input Description:
+# First line consists of input number
+
+# Output Description:
+# Output consists of “Even” or “oDd”
+
+# Sample Input :
+# 2
+# Sample Output :
+# Even
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+i=int(input())
+
+if i%2==0:
+    print("Even")
+else:
+    print("oDd")
