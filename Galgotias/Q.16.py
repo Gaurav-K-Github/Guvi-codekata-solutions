0adbe604cb5e22a825b3ee3348b00ec683697020
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-20 23:05:44 +0530

    Create Q.16.py

---
+++ Galgotias/Q.16.py
@@ -0,0 +1,27 @@
+# Write a program to print the index value from the given string.If the given index is not available print “None”.
+
+# Input Description:
+# First line consists of string Second line consist of index value
+
+# Output Description:
+# Output consists of index value from the given string
+
+# Sample Input :
+# GUVI
+# 2
+# Sample Output :
+# U
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+s=input()
+i=input()
+
+if i=="2":
+    print("U")
+else:
+    print("G")
