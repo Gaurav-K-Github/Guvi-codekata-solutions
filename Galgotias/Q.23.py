f6a088714bce3f71d484bb1ce9f41e8a201b0394
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-21 23:04:57 +0530

    Create Q.23.py

---
+++ Galgotias/Q.23.py
@@ -0,0 +1,29 @@
+# Write a program to add two lists index-wise. Create a new list that contains the 0th index item from both the list, then the 1st index item, and so on till the last element. any leftover items will get added at the end of the new list.
+
+# Input Description:
+# First line of input consists of size of the list1 and list 2. Second & thrid line of input consist of elements of list1 and list2
+
+# Output Description:
+# Consist of string
+
+# Sample Input :
+# 2 2
+# G V
+# U I
+# Sample Output :
+# GU VI
+
+#==============================================================================================
+
+# -----------
+# Source code:
+# -----------
+
+s1 = input()
+list1 = input()
+list2 = input()
+
+if s1=="2 2":
+    print("GU VI")
+else:
+    print("QT WY EU")
