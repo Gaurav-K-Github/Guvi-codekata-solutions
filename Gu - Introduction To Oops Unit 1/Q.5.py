df9942b33250554551ace64694ce2194e42dc6f2
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-05-24 00:02:06 +0530

    Update Q.5.py

--- Gu - Introduction To Oops Unit 1/Q.5.py
+++ Gu - Introduction To Oops Unit 1/Q.5.py
@@ -1,24 +1,24 @@
-# Vishakan, a dedicated reader, has a limited time to indulge in his favorite activity: reading books in the library. Each book has its own unique charm, but Vishakan needs to optimize his time to read as many books as possible within his available time.
+# On Earth, each plane has a special affinity for another plane. Given a group of n planes, numbered from 1 to n, where plane i likes plane fi, we aim to determine if any love triangle exists.
 
 # Input:
 
-# The first line contains two integers,(n) and (t) where, (1 <= n <= 10^5) and (1 <= t <= 10^9), denoting the number of books in the library and the total free minutes Vishakan has.
-# The second line contains( n) space-separated integers (a1, a2,...., an) where (1 <= ai <= 10^4), representing the time required to read each book.
+# The first line contains a single integer n (2 ≤ n ≤ 5000) — representing the number of planes.
+
+# The second line contains n integers f1, f2, ..., fn (1 ≤ fi ≤ n, fi ≠ i), indicating that the i-th plane is fond of plane fi.
+
 # Output:
 
-# Print a single integer, the maximum number of books Vishakan can read within his available time.
-# Constraints:
+# Output "YES" if there exists a love triangle among the planes. Otherwise, output "NO".
 
-# Vishakan has a maximum of( 10^9) minutes to spend in the library.
-# Each book takes between 1 and 10,000 minutes to read.
 # Sample input:
 
-# 3 3
-# 2 2 3
+# 5
+# 5 5 5 5 1
 
 # Sample output:
 
-# 1
+# NO
+
 
 #==============================================================================================
 
