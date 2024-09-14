ffdd8e37e5e9cc60ec1935f6b33f7b6f86bb822c
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-09-14 11:16:36 -0700

    Update Q.1.py

--- Absolute Beginner/Q.1.py
+++ Absolute Beginner/Q.1.py
@@ -1,7 +1,6 @@
 # You are provided with a number check whether its odd or even. 
 
 # Print "Odd" or "Even" for the corresponding cases.
-
 # Note: In case of a decimal, Round off to nearest integer and then find the output. Incase the input is zero, print "Zero".
 
 # Input Description:
